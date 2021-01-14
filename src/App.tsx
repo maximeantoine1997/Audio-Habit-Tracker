a0828ebb4fe6ff09553b96b2bf7375e3856dde97
maximeantoine1997 <maximeantoine1997@gmail.com>
2021-01-14 15:59:13 -0500

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
-
-import { Layout, Menu } from "antd";
-
 import {
   DesktopOutlined,
   PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined
+  SettingOutlined
 } from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import React, { useState } from "react";
 import "./index.css";
 
 const { Content, Footer, Sider } = Layout;
@@ -27,29 +23,17 @@ const App = () => {
         <div className="logo" />
         <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
           <Menu.Item key="1" icon={<PieChartOutlined />}>
-            Option 1
+            Dashboard
           </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Option 2
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9" icon={<FileOutlined />}>
-            Files
+          <Menu.Item key="2" icon={<SettingOutlined />}>
+            User Settings
           </Menu.Item>
         </Menu>
       </Sider>
       <Layout className="site-layout">
         <Content style={{ margin: "0 16px" }}></Content>
         <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
+          Created by Maxime Antoine ©2021
         </Footer>
       </Layout>
     </Layout>
