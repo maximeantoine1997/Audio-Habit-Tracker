befc4d78922c277b0d89f60bbb41df57aa4cc0fd
maximeantoine1997 <maximeantoine1997@gmail.com>
2021-01-14 14:33:52 -0500

    Install prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Audio-Habit-Tracker
+
 An audio habit tracker created with React - Firebase - DialogFlow
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import { DatePicker } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}> 
+      
+    </Layout>
   );
 }
 
