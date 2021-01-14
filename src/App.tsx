0f34e303ccf08560ac08b728b2ada9905c45490e
maximeantoine1997 <maximeantoine1997@gmail.com>
2021-01-14 15:54:08 -0500

    Add ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import { DatePicker } from 'antd';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React, { useState } from "react";
 
-const { Header, Content, Footer, Sider } = Layout;
+import { Layout, Menu } from "antd";
+
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined
+} from "@ant-design/icons";
+import "./index.css";
+
+const { Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
+const App = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-function App() {
   return (
-    <Layout style={{ minHeight: '100vh' }}> 
-      
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={isCollapsed}
+        onCollapse={(collapsed) => setIsCollapsed(collapsed)}
+      >
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Content style={{ margin: "0 16px" }}></Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     </Layout>
   );
-}
+};
 
 export default App;
