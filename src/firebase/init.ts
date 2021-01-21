192af8958e81c22a46d9543a16edec03bcba81d3
Maxime Antoine <maximeantoine1997@gmail.com>
2021-01-21 14:59:22 -0500

    Add firebase

---
+++ src/firebase/init.ts
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp78J1Fu0ZEQjgcGMVQNpe8-zgqxYKTdE",
+  authDomain: "audio-habit-tracker.firebaseapp.com",
+  projectId: "audio-habit-tracker",
+  storageBucket: "audio-habit-tracker.appspot.com",
+  messagingSenderId: "311556734602",
+  appId: "1:311556734602:web:ac1fe1bcb5b2fbf5f2b773"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
